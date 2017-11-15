8fe6f0973491d3deb881a80d3ca8f31e1cc3c83b
root <root@webserversl1.nacar.arg.igrupobbva>
2017-11-14 21:44:09 -0300

    Added

--- turnosapp/alertas_correo/enviarcorreo.sh
+++ turnosapp/alertas_correo/enviarcorreo.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+FECHA=$(date '+%Y-%m-%d %H:%M:%S')
 cd /home/produccion/sanlucas/
-python manage.py shell < turnosapp/alertas_correo/enviarcorreo.py
+echo "#############" >> /var/log/syslog
+echo $FECHA >> /var/log/syslog
+python manage.py shell < turnosapp/alertas_correo/enviarcorreo.py >> /var/log/syslog 2>&1
+echo "#############" >> /var/log/syslog
